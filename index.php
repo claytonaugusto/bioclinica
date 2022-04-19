5d9fcc3dbae5198b67cbd4bb2cd1dfe30d08e5c5
Clayton <relacionamento@ag29.com.br>
2022-04-19 15:47:21 -0300

    Página index pronta para receber formatação responsiva

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Laboratório Bioclínica</title>
+</head>
+
+<body>
+
+    <header>
+
+        <div class="content">
+            <div class="headerCenter">
+                <div class="logoTop">
+                    <a href="#"><img src="assets/images/logos/logotipo-bioclinica.png" alt="" srcset=""></a>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="#">Institucional</a></li>
+                        <li><a href="#">Exames</a></li>
+                        <li><a href="#">Convênios</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contato</a></li>
+                        <li class="btnNav"><a href="#">Resultados</a></li>
+
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+    </header>
+
+    <section class="slider">
+        aqui vai o carrosel
+    </section>
+
+    <section class="highlightsHome">
+        <div class="content">
+            <div class="highlightsTitle">
+                <h2>Usamos da mais alta tecnologia com a qualidade de mais de <strong>29 anos de experiência</strong>
+                </h2>
+            </div>
+            <div class="highlights">
+                <div class="highligthsBox">
+                    <div class="img">
+                        <img src="assets/images/icons/microscope.png" alt="" srcset="">
+                    </div>
+                    <h2>tecnologia de ponta</h2>
+                    <p>Em equipamentos e estrutura para garantia da excelência</p>
+                </div>
+                <div class="highligthsBox">
+                    <div class="img">
+                        <img src="assets/images/icons/smartphone.png" alt="" srcset="">
+                    </div>
+                    <h2>Resultados Online</h2>
+                    <p>Através so nosso site você tem acesso ao seu resultado de forma rápida e prática</p>
+                </div>
+                <div class="highligthsBox">
+                    <div class="img">
+                        <img src="assets/images/icons/heart.png" alt="" srcset="">
+                    </div>
+                    <h2>convênios</h2>
+                    <p>Além dos principais convênios, firmamos parcerias com empresas </p>
+                </div>
+                <div class="highligthsBox">
+                    <div class="img">
+                        <img src="assets/images/icons/children.png" alt="" srcset="">
+                    </div>
+                    <h2>coleta pediátrica</h2>
+                    <p>Trabalhamos para tornar mais confortável a experiência dos pequenos</p>
+                </div>
+                <div class="highligthsBox">
+                    <div class="img">
+                        <img src="assets/images/icons/medicine.png" alt="" srcset="">
+                    </div>
+                    <h2>competência e profissionalismo</h2>
+                    <p>Uma equipe capacitada e atualizada garante um tratamento diferenciado</p>
+                </div>
+                <div class="highligthsBox">
+                    <div class="img">
+                        <img src="assets/images/icons/conversation.png" alt="" srcset="">
+                    </div>
+                    <h2>orientação</h2>
+                    <p>Damos todas as orientações de pré-coleta sobre o tempo de jejum, a realização de exercícios
+                        físicos entre outros</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bannerHome">
+        <div class="content">
+            <div class="bannerHomeLeft">
+                <h2>Teste do</h2>
+                <h1>pezinho</h1>
+                <p>O teste do pezinho é um exame obrigatório para todos os recém-nascidos, que deve ser realizado após
+                    as primeiras 72 horas de vida.</p>
+                <button class="btnBannerHome"><a href="#">agendar</a></button>
+            </div>
+        </div>
+    </section>
+
+    <section class="checkUpHome">
+        <div class="content">
+            <div class="checkUp">
+                <div class="checkUpTitle">
+                    <h2>pacotes de check-up's feitos especialmente para você!</h2>
+                </div>
+                <div class="checkUpBox">
+                    <div class="img">
+                        <a href="#"><img src="assets/images/icons/drop.png" alt="" srcset=""></a>
+                    </div>
+                    <h2>check-up básico</h2>
+                    <p>Composto por 10 exames essenciais, entre eles: Hemograma completo, TGO, TGP, Glicose, entre
+                        outros</p>
+                </div>
+                <div class="checkUpBox">
+                    <div class="img">
+                        <a href="#"><img src="assets/images/icons/woman.png" alt="" srcset=""></a>
+                    </div>
+                    <h2>check-up mulher</h2>
+                    <p>Composto por 10 exames essenciais, entre eles: Hemograma completo, Lipidograma, Creatinina, entre
+                        outros</p>
+                </div>
+                <div class="checkUpBox">
+                    <div class="img">
+                        <a href="#"><img src="assets/images/icons/male.png" alt="" srcset=""></a>
+                    </div>
+                    <h2>check-up homem</h2>
+                    <p>Composto pelos principais exames, entre eles: Hemograma, Sumário de urina, Parasitológico, entre
+                        outros</p>
+                </div>
+                <div class="checkUpBox">
+                    <div class="img">
+                        <a href="#"><img src="assets/images/icons/baby.png" alt="" srcset=""></a>
+                    </div>
+                    <h2>check-up criança</h2>
+                    <p>Composto por 10 exames essenciais, entre eles: Hemograma completo, TGO, TGP, Glicose, entre
+                        outros</p>
+                </div>
+                <div class="checkUpBox">
+                    <div class="img">
+                        <a href="#"><img src="assets/images/icons/couple.png" alt="" srcset=""></a>
+                    </div>
+                    <h2>check-up pré-nupcial</h2>
+                    <p>Exames específicos como: Grupo sanguíneo, HBSAG (hepatite B), HCV, entre outros</p>
+                </div>
+                <div class="checkUpBox">
+                    <div class="img">
+                        <a href="#"><img src="assets/images/icons/pregnant.png" alt="" srcset=""></a>
+                    </div>
+                    <h2>check-up Gestante</h2>
+                    <p>Composto por 17 exames entre eles: Citomegalovírus, Toxoplasmose, entre outros</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="content">
+            <div class="footerInternal">
+                <div class="footerLeft">
+                    <a href="#"><img src="assets/images/logos/logotipo-bioclinica.png" alt="" srcset=""></a>
+                    <span><a href="#"><i class="fa-brands fa-instagram"></i></a></span>
+                    <span><a href="#"><i class="fa-brands fa-facebook"></a></i></span>
+
+                </div>
+                <div class="footerCenter">
+                    <ul>
+                        <li><a href="#">Institucional</a></li>
+                        <li><a href="#">Exames</a></li>
+                        <li><a href="#">Convênios</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contato</a></li>
+                        <li><a href="#">Resultados</a></li>
+                    </ul>
+                </div>
+                <div class="footerRight">
+                    <img src="assets/images/logos/pncq.png" alt="" srcset="">
+                    <img src="assets/images/logos/dicq.png" alt="" srcset="">
+                </div>
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
